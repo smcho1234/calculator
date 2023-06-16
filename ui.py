b7deebc99c9e49d7fb72c7f5d400c81f1ea733f0
dev_A <dev_A.email.com>
2023-06-16 15:25:48 +0900

    Add lbl1 to print ver. info

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
-                             QLineEdit, QComboBox)
+                             QLineEdit, QComboBox, QLabel)
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
 
@@ -15,6 +15,7 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
+        self.lbl1 = QLabel('v2.1.0', self)
         self.btn1=QPushButton('Calc',self)
         self.btn2=QPushButton('Clear',self)
         
@@ -35,7 +36,7 @@ class View(QWidget):
         hbox_formular.addWidget(self.le2)
         
         hbox = QHBoxLayout()
-        hbox.addStretch(1)
+        hbox.addWidget(self.lbl1)
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
         
@@ -52,9 +53,8 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
         
-
     def setDisplay(self, text):
         self.te1.appendPlainText(text)
-        
+      
     def clearMessage(self):
         self.te1.clear()
